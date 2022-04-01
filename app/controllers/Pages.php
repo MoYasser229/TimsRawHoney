928ef5ca0efd475365075788c56fa40786e31a9a
Mohammed Yasser <76665311+MoYasser229@users.noreply.github.com>
2022-04-01 19:42:23 +0200

    profile

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -105,6 +105,67 @@ class Pages extends Controller{
         session_destroy();
         redirect('pages/siginin');
     }
+    public function profile(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $model = $this->getModel();
+            
+            if(isset($_POST['submitPersonal'])){
+                $fname = $_POST['fname'];
+                $lname = $_POST['lname'];
+                $phone1 = $_POST['phone1'];
+                $phone2 = $_POST['phone2'];
+                $error = false;
+                if(empty($fname) || empty($lname) || empty($phone1) || empty($phone2))
+                    $error = true;
+                if(!$error){
+                    $model = $this->getModel();
+                    $result = $model->updatePersonal($_SESSION['ID'],$fname, $lname, $phone1,$phone2);
+                    if($result){
+                        redirect('pages/profile');
+                    }
+                    else
+                        echo "<script>alert('error')</script>";
+                }
+            }
+            if(isset($_POST['submitSecurity'])){
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                $error = false;
+                $validation = false;
 
+                if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+                    $validation = true;
+                    echo "<script>alert('Email is invalid')</script>";
+                }
+                if(empty($email) || empty($password))
+                    $error = true;
+
+                if(!$error && !$validation){
+                    $result = $model->updateSecurity($_SESSION['ID'],$email,$password);
+                    if($result){
+                        redirect('pages/profile');
+                    }
+                }
+                else if($error){
+                    echo "<script>alert('Email/Password is empty')</script>";
+                }
+            }
+            if(isset($_POST['submitAddress'])){
+                $address1 = $_POST['address'];
+                $address2 = $_POST['address2'];
+                $result = $model->updateAddress($_SESSION['ID'],$address1,$address2);
+                if($result)
+                    redirect('pages/profile');
+            }
+            if(isset($_POST['submitDelete'])){
+                $model->deleteAccount($_SESSION['ID']);
+                redirect('pages/index');
+            }
+        }
+        $profilePath = VIEWSPATH . 'pages/profile.php';
+        require_once $profilePath;
+        $profile = new Profile($this->getModel(), $this);
+        $profile->output();
+    }
     }
     
